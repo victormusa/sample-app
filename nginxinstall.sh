f4f45508c0602520809538c5c6bab7f544c88282
victormusa <victormusa@gmail.com>
2017-07-11 00:51:17 -0300

    Update nginxinstall.sh

--- nginxinstall.sh
+++ nginxinstall.sh
@@ -16,9 +16,12 @@ sudo sed -i 's/index index.html index.htm index.nginx-debian.html;/index index.p
 
 sudo sed -i 's#/var/www/html;#/var/www/html/sample-app;#g'  /etc/nginx/sites-available/default
 
+sudo sed -i 's#try_files $uri $uri/ =404;#try_files $uri $uri/index.php;#g'  /etc/nginx/sites-available/default
+
 sudo sed -i 's/# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000/location ~ \\.php$ { \n fastcgi_keep_conn on; \n include fastcgi_params; \n fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name; \n fastcgi_intercept_errors on; \n fastcgi_split_path_info ^(.+\\.php)(.*)$; \n fastcgi_hide_header X-Powered-By; \n fastcgi_pass 10.127.0.20:9000; \n }/g' /etc/nginx/sites-available/default
 
 sudo sed -i 's@#       deny all;@location ~ /\q.ht { \n                 deny all; \n         }@g'  /etc/nginx/sites-available/default
+
 
 #3.0 Reflect configuration:
 
